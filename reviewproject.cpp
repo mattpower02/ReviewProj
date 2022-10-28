c537ad58e110f9ce886fdd3161810ec2e4493ecb
andrewcampbell15 <90417648+andrewcampbell15@users.noreply.github.com>
2022-10-28 17:04:48 -0400

    Update reviewproject.cpp

--- reviewproject.cpp
+++ reviewproject.cpp
@@ -1,1 +1,28 @@
+#include "review.h"
+#include <iostream>
 
+void Student::name(std::string x) {
+    names = x;
+}
+void Student::assignment(int x) {
+        assignments = x;
+}
+
+void Student::weights(int A, int L, int P, int F) {
+        Aweights = A;
+        Lweights = L;
+        Pweights = P;
+        Fweights = F;
+}
+
+void Student::lab(int x) {
+        labs = x;
+}
+
+void Student::project(int x) {
+        projects = x;
+}
+
+void Student::final(int x) {
+        finals = x;
+}
