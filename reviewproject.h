f9f2025e7c7772bbe3dd1d218061c95a821d3b26
andrewcampbell15 <90417648+andrewcampbell15@users.noreply.github.com>
2022-10-28 17:05:06 -0400

    Update reviewproject.h

--- reviewproject.h
+++ reviewproject.h
@@ -1,1 +1,24 @@
+#include <iostream>
 
+class Student {
+    public:
+        std::string names;
+        float assignments;
+        float Aweights;
+        float labs;
+        float Lweights;
+        float projects;
+        float Pweights;
+        float finals;
+        float Fweights;
+        int finalGrade;
+
+        
+        void name(std::string x);
+        void assignment(int x);
+        void weights(int A, int L, int P, int F);
+        void project(int x);
+        void lab(int x);
+        void final(int x);
+
+};
